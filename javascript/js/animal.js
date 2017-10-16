506dcacbca589118aecdc317412d3c9b411d725d
Alina Shakya <alina.shakya3@gmail.com>
2017-10-17 00:21:46 +0100

    #master - adds alt method of animal display

--- javascript/js/animal.js
+++ javascript/js/animal.js
@@ -1,12 +1,9 @@
-
-const imageContainer = document.querySelector("#animal-result");
 function loadAnimals(animalName){
 	document.getElementById("animal-result").innerHTML = "";
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
     	if (this.readyState == 4 && this.status == 200) {
-    		var data = JSON.parse(this.responseText);
-    		jsonFlickrApi(data.photos.photo,animalName);
+    		jsonFlickrApi(JSON.parse(this.responseText).photos.photo,animalName);
     	}
   	};
   	xhttp.open("GET", "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key="+apiKey+"&tags="+animalName+"&format=json&page=1&per_page=5&nojsoncallback=?", true);
@@ -18,20 +15,6 @@ function jsonFlickrApi(response,animalName){
 		var image = document.createElement("img");
 		image.setAttribute("src", "https://farm"+val.farm+".staticflickr.com/"+val.server+"/"+val.id+"_"+val.secret+"_q.jpg");
     	image.setAttribute("alt", animalName);
-    	var src = document.getElementById("animal-result");
-		src.appendChild(image);
+		document.getElementById("animal-result").appendChild(image);
 	});
-}
-
-function createCORSRequest(method, url){
-    var xhr = new XMLHttpRequest();
-    if ("withCredentials" in xhr){
-        xhr.open(method, url, true);
-    } else if (typeof XDomainRequest != "undefined"){
-        xhr = new XDomainRequest();
-        xhr.open(method, url);
-    } else {
-        xhr = null;
-    }
-    return xhr;
 }
---
+++ javascript/js/animal2.js
@@ -0,0 +1,18 @@
+function jsonFlickrFeed(json){
+  Object.keys(json.items.slice(0,5)).forEach(function (key) {
+    var image = document.createElement('img');
+    image.setAttribute("src", json.items[key].media.m);
+    image.setAttribute("alt", "animal");
+    image.setAttribute("height", "150");
+    image.setAttribute("width", "150");
+    var src = document.getElementById("animal-result");
+    document.getElementById("animal-result").appendChild(image);
+  });
+}
+
+function loadAnimals(animalName){
+  document.getElementById("animal-result").innerHTML = "";
+  var script = document.createElement('script');
+  script.src = "https://api.flickr.com/services/feeds/photos_public.gne?tags="+animalName+"&format=json";
+  document.getElementById("animal-result").appendChild(script);
+}
