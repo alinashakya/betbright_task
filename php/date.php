77c74143c2c2a1133bbe462b57bc0787dac6d38e
Alina Shakya <alina.shakya3@gmail.com>
2017-10-17 21:01:10 +0100

    #master - changes in date functions

--- php/date.php
+++ php/date.php
@@ -1,20 +1,53 @@
 <?php
 
-function getUpcomingDates($date){
-	$currentDay = date('l');
-	$compareDate = date('Y-m-d 20:00:00');
-	$wednesday = (strtotime($compareDate) > strtotime($date) && $currentDay == 'Wednesday')?$date:date('Y-m-d', strtotime('next tuesday', strtotime($date)));
-	$saturday = (strtotime($compareDate) > strtotime($date) && $currentDay == 'Saturday')?$date:date('Y-m-d', strtotime('next saturday', strtotime($date)));
-	
-	if(strtotime($wednesday) < strtotime($saturday)){
-		echo "Given Date: $date <br/>Next valid Irish lottery draw dates are $wednesday (Wednesday) and $saturday (Saturday)";
-	}else{
-		echo "Given Date : $date <br/>Next valid Irish lottery draw dates are: $saturday (Saturday) and $wednesday (Wednesday)";
-	}
-	
+/**
+ * Function used to get upcoming lottery dates
+ * @param date $date 
+ * @return array $value array of recent two lottery dates
+ */
+function getUpcomingDates($date)
+{
+    $currentDay = date('l');
+    $wednesday  = (checkForToday($date, 'Wednesday')) ? $date : nextDay($date, 'wednesday');
+    $saturday   = (checkForToday($date, 'Saturday')) ? $date : nextDay($date, 'saturday');
+    $value      = array(
+        $wednesday,
+        $saturday
+    );
+    asort($value);
+    return $value;
 }
 
-$givenDate = $_GET['givenDate'];
+/**
+ * Function used to check if date provided is equals to day provided
+ * @param date $date 
+ * @param day $day
+ * @return boolean 
+ */
+function checkForToday($date, $currentDay)
+{
+    $compareDate = date('Y-m-d 20:00:00');
+    $today       = date('l');
+    return strtotime($compareDate) > strtotime($date) && $currentDay == $today;
+}
+
+/**
+ * Function used to get date of next occurence of given day
+ * @param date $date 
+ * @param day $day 
+ * @return date
+ */
+function nextDay($date, $day)
+{
+    $day = 'next ' . strtolower($day);
+    return date('Y-m-d', strtotime($day, strtotime($date)));
+}
+
+$givenDate   = isset($_GET['givenDate']) ? $_GET['givenDate'] : '';
 $currentDate = date('Y-m-d H:i:s');
-$date = (!empty($givenDate))?$givenDate:$currentDate;
-getUpcomingDates($date);
+$date        = (!empty($givenDate)) ? $givenDate : $currentDate;
+$dates       = getUpcomingDates($date);
+echo "Next valid Irish lottery draw dates are: <br/>";
+foreach ($dates as $key => $val) {
+    echo date_format(date_create($val),'Y-m-d, l') . '<br/>';
+}
