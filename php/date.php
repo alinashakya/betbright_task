934f75ff5e5e1611b3818cfdc5e2b848125236d9
Alina Shakya <alina.shakya3@gmail.com>
2017-10-17 18:26:13 +0100

    #master - php date text changes

--- php/date.php
+++ php/date.php
@@ -2,14 +2,14 @@
 
 function getUpcomingDates($date){
 	$currentDay = date('l');
-	$compareDate = date('Y-m-d 16:00:00');
-	$wednesday = (strtotime($compareDate) > strtotime($date) && $currentDay == 'Tuesday')?$date:date('Y-m-d', strtotime('next tuesday', strtotime($date)));
+	$compareDate = date('Y-m-d 20:00:00');
+	$wednesday = (strtotime($compareDate) > strtotime($date) && $currentDay == 'Wednesday')?$date:date('Y-m-d', strtotime('next tuesday', strtotime($date)));
 	$saturday = (strtotime($compareDate) > strtotime($date) && $currentDay == 'Saturday')?$date:date('Y-m-d', strtotime('next saturday', strtotime($date)));
 	
 	if(strtotime($wednesday) < strtotime($saturday)){
-		echo "Next valid draw dates are :".$wednesday." (Wednesday) and ".$saturday." (Saturday)";
+		echo "Given Date: $date <br/>Next valid Irish lottery draw dates are $wednesday (Wednesday) and $saturday (Saturday)";
 	}else{
-		echo "Next valid draw dates are :".$saturday." (Saturday) and ".$wednesday." (Wednesday)";
+		echo "Given Date : $date <br/>Next valid Irish lottery draw dates are: $saturday (Saturday) and $wednesday (Wednesday)";
 	}
 	
 }
